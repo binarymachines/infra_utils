b45da20bd4c8a0d9871dd2d69b6d091cd7444654
Dexter Taylor <binarymachineshop@gmail.com>
2022-05-16 15:45:49 -0400

    Add files via upload

---
+++ install_python_3.9.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo apt install software-properties-common -y
+sudo add-apt-repository ppa:deadsnakes/ppa -y
+sudo apt update
+sudo apt install python3.9 -y
+sudo apt install python3.9-dev -y
+sudo apt install python3.9-distutils -y
---
+++ setpyversion.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.9 10
+
